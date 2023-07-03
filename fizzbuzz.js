c1b24f16700f1dc5b550aebccb24a17ffaa64a74
Alison539 <alisonmdauris@gmail.com>
2023-07-03 17:23:14 +0100

    Basic fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,6 +3,25 @@ function fizzbuzz() {
     console.log("Hello, World!");
 
     // Put your code here...
+    for (let i = 0; i < 101; i++) {
+        let toPrint = "";
+        let special = false;
+        if(i % 3 == 0){
+            toPrint += "Fizz";
+            special = true;
+        }
+        if (i%5 == 0){
+            toPrint += "Buzz";
+            special = true;
+        }
+        if (special){
+            console.log(toPrint)
+        }
+        else{
+            console.log(i)
+        }
+        
+    }
 
 }
 
