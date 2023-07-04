ab030c06ca7247282ed67076bede6d9a840a72ad
Alison539 <alisonmdauris@gmail.com>
2023-07-04 09:47:21 +0100

    Added user input as well as fizzbuzzbangbongfezz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,40 @@
+var readline = require('readline-sync');
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
+    console.log("Hello, please enter some input: ");
+    const response = readline.prompt();
 
+    let codes = {
+        3: "Fizz",
+        5: "Buzz",
+        7: "Bang",
+        11: "Bong",
+        13: "Fezz"
+    }
     // Put your code here...
-    for (let i = 0; i < 101; i++) {
+    for (let i = 0; i < response; i++) {
         let toPrint = "";
         let special = false;
-        if(i % 3 == 0){
-            toPrint += "Fizz";
-            special = true;
+
+        //checking if need to reverse the order
+        if(i % 17 == 0){
+            for (const [key, value] of Object.entries(codes).reverse()) {
+                if (i%key == 0){
+                 toPrint += value;
+                 special = true;
+                }
+            }
         }
-        if (i%5 == 0){
-            toPrint += "Buzz";
-            special = true;
+        else{
+            //normal order
+            for (const [key, value] of Object.entries(codes)) {
+                if (i%key == 0){
+                 toPrint += value;
+                 special = true;
+                }
+            }
         }
+        
         if (special){
             console.log(toPrint)
         }
