519682ccdefcc7df506fe325e9807b02c93926cd
Alison539 <alisonmdauris@gmail.com>
2023-07-04 14:40:37 +0100

    Making code neater (and correcting errors)

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,16 @@
 var readline = require('readline-sync');
 // This is our main function
 function fizzbuzz() {
+    
+    // getting user input
     console.log("Please enter max number:");
     const maxNum = readline.prompt();
-    console.log("Hello, please enter which rules to implement (which combination of 3, 5, 7, 11, 13, 17)  - input as comma separated");
+    console.log("Hello, please enter which rules to implement (which combination of 3, 5, 7, 11, 13, 17)  - input as comma separated and in ascending order");
     const response = readline.prompt();
-    var rules = JSON.parse("[" + response + "]").sort();
+    
+    // preparing the user input
+    var rules = JSON.parse("[" + response + "]");
+
     if(13 in rules){
         rules.splice(rules.indexOf(13),1)
         if (3 in rules){
@@ -15,7 +20,6 @@ function fizzbuzz() {
             rules.splice(0,0,13)
         }
     }
-
 
     let codes = {
         3: "Fizz",
@@ -27,55 +31,51 @@ function fizzbuzz() {
     // Put your code here...
     for (let i = 1; i < +maxNum + 1; i++) {
         let toPrint = "";
-        let special = false;
-
-        if (rules.includes(17) && i%17 == 0){
-            //checking if need to reverse the order
-            for (let j = 0; j < rules.length - 1; j++) {
-                key = rules[j]
-                if (rules.includes(+key)){
-                    if (i%key == 0){
-                        toPrint += codes[key];
-                        special = true;
-                    }
-                }
+        let special = false; //this variable symbolises if the number is a multiple of one of the specified numbers
+        let reverse = false;
+        
+        
+        if (rules.includes(17)){
+            if (i%17 ==0){
+                rules = rules.reverse()
+                reverse= true;
             }
-            if(rules.includes(11) && i%11 == 0){
-                toPrint = "Bong";
-                special = true
-                if(rules.includes(13) && i%13 ==0){
+            rules.splice(rules.indexOf(17),1)
+        }
+        //normal order
+        for (let j = 0; j < rules.length - 1; j++) {
+            key = rules[j]
+            
+            if (i%key == 0){
+                toPrint += codes[key];
+                special = true;
+            }
+        
+        }
+        if(rules.includes(11) && i%11 == 0){
+            toPrint = "Bong";
+            special = true
+            if(rules.includes(13) && i%13 ==0){
+                if(reverse){
                     toPrint = "BongFezz"
                 }
-            }
-        }
-
-        else{
-            //normal order
-            for (let j = 0; j < rules.length - 1; j++) {
-                key = rules[j]
-                if (rules.includes(+key)){
-                    if (i%key == 0){
-                        toPrint += codes[key];
-                        special = true;
-                    }
-                }
-            }
-            if(rules.includes(11) && i%11 == 0){
-                toPrint = "Bong";
-                special = true
-                if(rules.includes(13) && i%13 ==0){
+                else{
                     toPrint = "FezzBong"
-                    
                 }
-                
             }
+            
         }
+            
        
         if (special){
             console.log(toPrint)
         }
         else{
             console.log(i)
+        }
+
+        if(reverse){
+            rules = rules.reverse(); //undoing the reversal
         }
     }
         
